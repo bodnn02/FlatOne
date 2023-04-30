403036724c14bd13766e5666d3359243c09c54ba
bodnn02 <bodnn03@gmail.com>
2023-04-30 21:55:57 +0300

    new file:   article.html 	new file:   articles.html 	new file:   catalogue.html 	new file:   css/fonts.css 	new file:   css/jquery-ui.min.css 	new file:   css/reset.css 	new file:   css/style.css 	new file:   fonts/Inter-VariableFont_slnt,wght.ttf 	new file:   fonts/Raleway-VariableFont_wght.ttf 	new file:   img/animals.svg 	new file:   img/apartament-preview.jpg 	new file:   img/arrow-left.svg 	new file:   img/article-banner.jpg 	new file:   img/article-form.jpg 	new file:   img/article-gallery-1.jpg 	new file:   img/article-gallery-2.jpg 	new file:   img/article-gallery-3.jpg 	new file:   img/article-gallery-4.jpg 	new file:   img/article-gallery-5.jpg 	new file:   img/article-preview.jpg 	new file:   img/banner-arrow.svg 	new file:   img/banner-product.jpg 	new file:   img/bed.svg 	new file:   img/breadcrumbs-arrow.svg 	new file:   img/call.svg 	new file:   img/catalogue-banner.png 	new file:   img/catalogue-header.jpg 	new file:   img/chat.svg 	new file:   img/checkmark.svg 	new file:   img/children.svg 	new file:   img/close-white.svg 	new file:   img/close.svg 	new file:   img/conditioner.svg 	new file:   img/crate-lift.svg 	new file:   img/cross-white.svg 	new file:   img/cross.svg 	new file:   img/expand-icon.svg 	new file:   img/fireplace.svg 	new file:   img/gallery-arrow.svg 	new file:   img/gallery-overlay.jpg 	new file:   img/game.svg 	new file:   img/heart-filled.svg 	new file:   img/heart.svg 	new file:   img/instagram.svg 	new file:   img/learn-more.svg 	new file:   img/lift.svg 	new file:   img/link-arrow.svg 	new file:   img/lock.svg 	new file:   img/logo-white.svg 	new file:   img/logo.svg 	new file:   img/love.svg 	new file:   img/main-background.jpg 	new file:   img/metro.svg 	new file:   img/musics.svg 	new file:   img/our-mission.jpg 	new file:   img/pdf.svg 	new file:   img/product-image.jpg 	new file:   img/safe.svg 	new file:   img/search.svg 	new file:   img/sections-preview.jpg 	new file:   img/share.svg 	new file:   img/sleep.svg 	new file:   img/territory.svg 	new file:   img/toilet.svg 	new file:   img/try-preview.jpg 	new file:   img/vk.svg 	new file:   img/washing.svg 	new file:   img/whatsup.svg 	new file:   img/wifi.svg 	new file:   "img/\320\263\320\273\320\260\320\262\320\275\321\213\320\271 \321\204\320\276\320\275.jpg" 	new file:   index.html 	new file:   js/jquery-ui.min.js 	new file:   js/jquery.min.js 	new file:   js/main.js 	new file:   our-staff.html 	new file:   product-item.html 	new file:   saved-list.html

---
+++ js/main.js
@@ -0,0 +1,224 @@
+$(".preview-paggination__item").on("click", function (e) {
+  gallery = $(this).parent().parent().find(".apartament-list__preview").children("img")
+
+  $(this).parent().children(".preview-paggination__item").removeClass("selected");
+  $(this).addClass("selected");
+
+  gallery.removeClass("current")
+  gallery.eq($(this).index()).addClass("current")
+});
+
+$(".filter-list__item").on("click", function (e) {
+  if($(this).parent().hasClass("single")) {
+    $(this).parent().children(".filter-list__item").removeClass("selected");
+    $(this).addClass("selected");
+  } else {
+    $(this).toggleClass("selected");
+  }
+});
+$(".filter-section__header").on("click", function (e) {
+  $(this).parent().toggleClass("opened");
+});
+$(".apartament-list__favorite-btn").on("click", function (e) {
+  $(this).toggleClass("filled");
+});
+
+
+var minSpan = $( ".price-range__min-span" );
+var maxSpan = $( ".price-range__max-span" );
+
+$(".price-range").slider({
+  range: true,
+  min: 0,
+  max: 500,
+  values: [75, 300],
+  slide: function (event, ui) {
+    $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+  },
+});
+
+
+$( function() {
+    var minPrice = 500;
+    var maxPrice = 57000;
+    $( ".price-range" ).each( function() {
+        var minSpan = $( this ).find( ".price-range__min" );
+        var maxSpan = $( this ).find( ".price-range__max" );
+        $( this ).slider({
+            range: true,
+            min: minPrice,
+            max: maxPrice,
+            values: [ minPrice, maxPrice ],
+            slide: function( event, ui ) {
+                minSpan.text( ui.values[ 0 ] + "₽" );
+                maxSpan.text( ui.values[ 1 ] + "₽" );
+            }
+        });
+        minSpan.text( $( this ).slider( "values", 0 ) + "₽" );
+        maxSpan.text( $( this ).slider( "values", 1 ) + "₽" );
+    });
+});
+
+$( function() {
+  $(".datepicker").datepicker();
+} );
+
+
+$(".try-paggination__item").on("click", function (e) {
+  lead_lists = $(this).parent().parent().find(".lead-wrapper").children(".lead-list");
+
+  $(this).parent().children(".try-paggination__item").removeClass("selected");
+  $(this).addClass("selected");
+
+  lead_lists.removeClass("current")
+  lead_lists.eq($(this).index()).addClass("current")
+});
+
+$(".mm-btn").on("click", function (e) {
+  $(".m-menu").addClass("opened")
+
+  $('body').css('overflow', 'hidden');
+});
+$(".m-menu__close").on("click", function (e) {
+  $(".m-menu").removeClass("opened")
+  $('body').css('overflow', 'visible');
+});
+
+$("#m-filters").on("click", function (e) {
+  $(".catalogue-sidebar").addClass("opened")
+
+  $('body').css('overflow', 'hidden');
+});
+$(".catalogue-sidebar__m-close").on("click", function (e) {
+  $(".catalogue-sidebar").removeClass("opened")
+  
+  $('body').css('overflow', 'visible');
+});
+
+
+$(window).on('load', function() {
+  $('.preloader').delay(1000).fadeOut('slow');
+});
+
+$(window).on('load', function() {
+  var $preloader = $('.preloader'),
+      $line = $preloader.find('.loading-bar__line'),
+      $percent = $preloader.find('.loading-bar__percent'),
+      percent = 0,
+      interval = setInterval(function() {
+          percent += Math.floor(Math.random() * 25) + 1;
+          if (percent >= 100) {
+              percent = 100;
+              clearInterval(interval);
+              $preloader.delay(1000).fadeOut('slow');
+          }
+          if(percent>=25) {
+            $(".preloader__title").text("покупка")
+            $(".preloader__text").text("Апартаментов / офисов / парковок")
+          }
+          if(percent>=50) {
+            $(".preloader__title").text("субаренда")
+            $(".preloader__text").text("Апартаментов / офисов")
+          }
+          if(percent>=75) {
+            $(".preloader__title").text("Flat one")
+            $(".preloader__text").text("Инвестиционная недвижимость Москвы и Москвы сити")
+          }
+          $line.css('width', percent + '%');
+          $percent.text(percent + '%');
+      }, 500);
+});
+
+$(".gallery-overlay__item").on("click", function (e) {
+  gallery_current = $(this).parent().parent().parent().find(".gallery-overlay__current");
+
+  $(this).parent().children(".gallery-overlay__item").removeClass("selected");
+  $(this).toggleClass("selected");
+
+  gallery_current.attr('src', $(this).children("img").attr('src'));
+});
+
+$(".gallery-overlay__prev").on("click", function (e) {
+  gallery_current = $(this).parent().parent().find(".gallery-overlay__current")
+  paggination_list = $(this).parent().parent().find(".gallery-overlay__list")
+
+  current_index = $(paggination_list).children(".selected").index()
+  lenght_list = $(paggination_list).children("li").length
+
+  if (current_index == 0) {
+    $(paggination_list).children("li").removeClass("selected")
+    $(paggination_list).children("li").eq(lenght_list).addClass("selected")
+    gallery_current.attr('src', $(paggination_list).children("li").children("img").attr("src"))
+  }
+  if(current_index == lenght_list) {
+    $(paggination_list).children("li").removeClass("selected")
+    $(paggination_list).children("li").eq(0).addClass("selected")
+    gallery_current.attr('src', $(paggination_list).children("li").eq(0).children("img").attr("src"))
+  }
+  if(current_index < lenght_list) {
+    $(paggination_list).children("li").removeClass("selected")
+    $(paggination_list).children("li").eq(current_index - 1).addClass("selected")
+    gallery_current.attr('src', $(paggination_list).children("li").eq(current_index - 1).children("img").attr('src'))
+  }
+});
+
+$(".gallery-overlay__next").on("click", function (e) {
+  gallery_current = $(this).parent().parent().find(".gallery-overlay__current")
+  paggination_list = $(this).parent().parent().find(".gallery-overlay__list")
+  
+  current_index = $(paggination_list).children(".selected").index()
+  lenght_list = $(paggination_list).children("li").length
+
+  if (current_index == 0) {
+    $(paggination_list).children("li").removeClass("selected")
+    $(paggination_list).children("li").eq(lenght_list).addClass("selected")
+
+    gallery_current.attr('src', $(paggination_list).children("li").children("img").attr("src"))
+  }
+  if(current_index == lenght_list) {
+    $(paggination_list).children("li").removeClass("selected")
+    $(paggination_list).children("li").eq(0).addClass("selected")
+
+    gallery_current.attr('src', $(paggination_list).children("li").eq(0).children("img").attr("src"))
+  }
+  if(current_index < lenght_list) {
+    $(paggination_list).children("li").removeClass("selected")
+    $(paggination_list).children("li").eq(current_index + 1).addClass("selected")
+
+    gallery_current.attr('src', $(paggination_list).children("li").eq(current_index + 1).children("img").attr('src'))
+  }
+});
+
+$(".product-gallery__see-all").on("click", function (e) {
+  $(".overlay").addClass("opened")
+  $(".gallery-overlay").addClass("opened")
+});
+$(".gallery-overlay__close").on("click", function (e) {
+  $(".overlay").removeClass("opened")
+  $(".gallery-overlay").removeClass("opened")
+});
+
+$("#book-btn").on("click", function (e) {
+  $('body').css('overflow', 'hidden');
+
+  $(".overlay").addClass("opened")
+  $(".form-overlay").addClass("opened")
+});
+$(".form-overlay__close").on("click", function (e) {
+  $('body').css('overflow', 'visible');
+
+  $(".overlay").removeClass("opened")
+  $(".form-overlay").removeClass("opened")
+});
+
+$(".form-overlay__back-btn").on("click", function (e) {
+  $('body').css('overflow', 'visible');
+
+  $(".overlay").removeClass("opened")
+  $(".form-overlay").removeClass("opened")
+  return false;
+});
+
+$(".wishlist-btn").on("click", function (e) {
+  $(this).toggleClass("filled")
+});
